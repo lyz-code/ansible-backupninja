c66898d14dd9112f4624b229d9edb4a154381018
Lyz <lyz@riseup.net>
2018-04-28 11:45:59 +0200

    First iteration

---
+++ README.md
@@ -0,0 +1,46 @@
+Backupninja
+=========
+
+The intended use of this role is the installation backupninja and do a backup test based in La Brecha Digital needs.
+
+Requirements
+------------
+
+* Debian based hosts
+
+Role Variables
+--------------
+* `ninja`: Dictionary with the options of the role
+  * `email_to_report`: Email to send status of the logs
+  * `backup_retention_period`: Period of retention of backups (Default: `60D`)
+  * `backup_method`: Select the backup method to use (Default: `rdiff`)
+  * `include`: List of directories to be included in the backup
+  * `exclude`: List of directories to be excluded from the backup
+  * `temp_backup_dir`: The directory where the temporal files for the backup
+    are going to be stored
+  * `backup_dir`: The directory where the backup is going to be stored
+
+Dependencies
+------------
+
+None.
+
+Example Playbook
+----------------
+
+```yaml
+---
+- hosts: all
+  roles:
+    - backupninja
+```
+
+License
+-------
+
+GPLv2
+
+Author Information
+------------------
+
+Lyz (lyz at riseup net)
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,92 @@
+import os
+import pytest
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("package", [
+    ("backupninja"),
+    ("hwinfo"),
+    ("debconf-utils"),
+    ("flashrom"),
+    ("rdiff-backup"),
+    ("postfix"),
+])
+def test_required_packages_are_installed(host, package):
+    assert host.package(package).is_installed
+
+
+@pytest.mark.parametrize("directories", [
+    ("/backups"),
+    ("/var/backups"),
+])
+def test_required_directories_exist(host, directories):
+    file = host.file(directories)
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert oct(file.mode) == '0755'
+
+
+def test_ninja_global_config(host):
+    file = host.file('/etc/backupninja.conf')
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert oct(file.mode) == '0600'
+    assert file.contains('reportemail = lyz@gentooza.me')
+
+
+def test_ninja_sys_backup_config(host):
+    file = host.file('/etc/backup.d/10.sys')
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert oct(file.mode) == '0600'
+
+
+@pytest.mark.parametrize("sys_files", [
+    ("/var/backups/debconfsel.txt"),
+    ("/var/backups/dpkg-selections.txt"),
+    ("/var/backups/hardware.txt"),
+    ("/var/backups/sysreport.txt"),
+])
+def test_ninja_sys_files_were_created_(host, sys_files):
+    file = host.file(sys_files)
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert oct(file.mode) == '0600'
+
+
+def test_ninja_rdiff_backup_config(host):
+    file = host.file('/etc/backup.d/20.rdiff')
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert oct(file.mode) == '0600'
+    assert file.contains('nicelevel = 19')
+    assert file.contains('label = backupninja_debian_stretch')
+    assert file.contains('type = local')
+    assert file.contains('keep = 60D')
+    assert file.contains("include = /var/spool/cron/crontabs")
+    assert file.contains("include = /var/backups")
+    assert file.contains("include = /etc")
+    assert file.contains("include = /root")
+    assert file.contains("include = /home")
+    assert file.contains("include = /usr/local/bin")
+    assert file.contains("include = /usr/local/sbin")
+    assert file.contains("include = /var/lib/dpkg/status")
+    assert file.contains("include = /var/lib/dpkg/status-old")
+    assert file.contains("exclude = /tmp")
+    assert file.contains("directory = /backups")
+
+
+def test_ninja_rdiff_worked(host):
+    file = host.file('/backups/backupninja_debian_stretch')
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert oct(file.mode) == '0755'
